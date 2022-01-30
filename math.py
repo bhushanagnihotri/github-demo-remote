d2cef4a45c1fb7f3a9a6f7775fd3c7bd02cf2301
bhushanagnihotri <bhushanpagnihotri987@gmail.com>
2022-01-30 18:52:03 +0530

    Implementation of multiply Function from local repository

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
 	return(x-y); #from remote repository
 #multiply Implementation	
 def multiply(x,y):
-	pass
+	return(x*y); #from local repository
 #divide implementation	
 def divide(x,y):
 	pass
